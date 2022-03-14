f1341ed16eb3d2098f26e2c5473ee83c89c67b18
rodriguezespinoza0795 <rodriguezespinoza0795@gmail.com>
2022-03-14 14:40:34 -0600

    Argumentos

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const { readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolvers')
 
-const schema = buildSchema(readFileSync(join(__dirname,'lib','schema.graphql'), 'utf8'));
+const typeDefs = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf8'));
+
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 const app = express();
 const port = process.env.port || 4000
