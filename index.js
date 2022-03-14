5e3f8baf97046d4caa02f81579c68d654945ee1f
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-03-14 12:41:18 -0600

    express-graphql

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const { graphql, buildSchema } = require('graphql')
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-// definiendo Schema
 const schema = buildSchema(`
-	type Query {
-		hello: String
-	} 
+  type Query {
+    hello: String
+  }
 `);
 
-// definiciendo los Resolvers
+const app = express();
+const port = process.env.port || 4000
+
 const resolvers = {
     hello: () => {
         return 'Hola Mundo'
     }
 }
 
-// Ejecutar el query hello
-graphql({
-    schema: schema,
-    source: '{ hello }',
-    rootValue: resolvers
-})
-.then((data) => {console.log(data)})
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true,
+}));
+
+app.listen(port, () => console.log(`Now browse to localhost:${port}/graphql`));
