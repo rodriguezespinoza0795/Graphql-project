d7ad84c3569605dd275b6bddd4b3b16450d2aa40
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-03-14 12:21:37 -0600

    Queries y Resolvers

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const { graphql, buildSchema } = require('graphql')
 
-// definiciendo Schema
+// definiendo Schema
 const schema = buildSchema(`
 	type Query {
 		hello: String
 	} 
 `);
 
+// definiciendo los Resolvers
+const resolvers = {
+    hello: () => {
+        return 'Hola Mundo'
+    }
+}
+
 // Ejecutar el query hello
-graphql({schema:schema,source:'{hello}'})
+graphql({
+    schema: schema,
+    source: '{ hello }',
+    rootValue: resolvers
+})
 .then((data) => {console.log(data)})
