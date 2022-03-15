71a52d594a5f95e67a179b079596be6d82625029
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-03-15 11:30:18 -0600

    Preparando API para producción

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require("cors")
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
@@ -11,12 +12,14 @@ const typeDefs = buildSchema(readFileSync(join(__dirname, 'lib', 'schema.graphql
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 const app = express();
+app.use(cors())
 const port = process.env.port || 4000
+const isDev = process.env.NODE_ENV !== 'production'
 
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
-  graphiql: true,
+  graphiql: isDev,
 }));
 
 app.listen(port, () => console.log(`Now browse to localhost:${port}/graphql`));
